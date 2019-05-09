43de90bc4406e491600d41bf4ae0378286993b15
wheniturnintoamartian <steve.papadogiannis1992@gmail.com>
2019-05-09 12:39:33 +0300

    Adding two tests to register handler

--- test/Custom/Auth/Email/Handler/RegisterSpec.hs
+++ test/Custom/Auth/Email/Handler/RegisterSpec.hs
@@ -7,8 +7,42 @@ import TestImport
 
 spec :: Spec
 spec = withApp $ do
-  describe "Post request to http://localhost:3000/auth/plugin/email/register" $
-    it "gives a 200 and the body contains \"message\":\"Password must be at least three characters\"" $ do
+  describe "Post request to http://localhost:3000/auth/plugin/email/register" $ do
+    it "with only email in request body gives a 200 and the response body contains \"message\":\"No password provided\"" $ do
+      get ("http://localhost:3000/auth/check" :: Text)
+      statusIs 200
+
+      let email = "example@gmail.com" :: Text
+          body = object [ "email" .= email ]
+          encoded = encode body
+
+      request $ do
+        setMethod "POST"
+        setUrl $ AuthR $ PluginR "email" ["register"]
+        setRequestBody encoded
+        addRequestHeader ("Content-Type", "application/json")
+        addTokenFromCookie
+
+      statusIs 200
+      bodyContains "\"message\":\"No password provided\""
+    it "with only email in request body gives a 200 and the response body contains \"message\":\"No email provided\"" $ do
+      get ("http://localhost:3000/auth/check" :: Text)
+      statusIs 200
+
+      let password = "pa" :: Text
+          body = object [ "password" .= password ]
+          encoded = encode body
+
+      request $ do
+        setMethod "POST"
+        setUrl $ AuthR $ PluginR "email" ["register"]
+        setRequestBody encoded
+        addRequestHeader ("Content-Type", "application/json")
+        addTokenFromCookie
+
+      statusIs 200
+      bodyContains "\"message\":\"No email provided\""
+    it "with password \"pa\" gives a 200 and the body contains \"message\":\"Password must be at least three characters\"" $ do
       get ("http://localhost:3000/auth/check" :: Text)
       statusIs 200
 
