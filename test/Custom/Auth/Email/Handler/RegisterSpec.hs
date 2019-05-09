f363a52840c418c5edaeb6984ef4450c23cac0e5
wheniturnintoamartian <steve.papadogiannis1992@gmail.com>
2019-05-09 12:46:59 +0300

    Adding three more tests

--- test/Custom/Auth/Email/Handler/RegisterSpec.hs
+++ test/Custom/Auth/Email/Handler/RegisterSpec.hs
@@ -60,7 +60,61 @@ spec = withApp $ do
 
       statusIs 200
       bodyContains "\"message\":\"Password must be at least three characters\""
+    it "with invalid email and password gives a 200 and the body contains \"message\":\"Invalid email address provided\"" $ do
+      get ("http://localhost:3000/auth/check" :: Text)
+      statusIs 200
 
+      let email = "examplegmail.com" :: Text
+          password = "password" :: Text
+          body = object [ "email" .= email, "password" .= password ]
+          encoded = encode body
+
+      request $ do
+        setMethod "POST"
+        setUrl $ AuthR $ PluginR "email" ["register"]
+        setRequestBody encoded
+        addRequestHeader ("Content-Type", "application/json")
+        addTokenFromCookie
+
+      statusIs 200
+      bodyContains "\"message\":\"Invalid email address provided\""
+    it "with valid email and password gives a 200 and the body contains \"message\":\"A confirmation e-mail has been sent to example@gmail.com.\"" $ do
+      get ("http://localhost:3000/auth/check" :: Text)
+      statusIs 200
+
+      let email = "example@gmail.com" :: Text
+          password = "password" :: Text
+          body = object [ "email" .= email, "password" .= password ]
+          encoded = encode body
+
+      request $ do
+        setMethod "POST"
+        setUrl $ AuthR $ PluginR "email" ["register"]
+        setRequestBody encoded
+        addRequestHeader ("Content-Type", "application/json")
+        addTokenFromCookie
+
+      statusIs 200
+      bodyContains "\"message\":\"A confirmation e-mail has been sent to example@gmail.com.\""
+    it "with capital containing email and password gives a 200 and the body contains \"message\":\"A confirmation e-mail has been sent to example@gmail.com.\"" $ do
+      get ("http://localhost:3000/auth/check" :: Text)
+      statusIs 200
+
+      let email = "EXAMPLE@gmail.com" :: Text
+          password = "password" :: Text
+          body = object [ "email" .= email, "password" .= password ]
+          encoded = encode body
+
+      request $ do
+        setMethod "POST"
+        setUrl $ AuthR $ PluginR "email" ["register"]
+        setRequestBody encoded
+        addRequestHeader ("Content-Type", "application/json")
+        addTokenFromCookie
+
+      statusIs 200
+      bodyContains "\"message\":\"A confirmation e-mail has been sent to example@gmail.com.\""
+    
   describe "Get request to http://localhost:3000/auth/plugin/email/register" $
     it "gives a 404 Not Found" $ do
       get ("http://localhost:3000/auth/plugin/email/register" :: Text)
