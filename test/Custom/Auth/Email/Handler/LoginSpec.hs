cd59cdae2859692ab2ef8b62eee37e196cefd48c
wheniturnintoamartian <steve.papadogiannis1992@gmail.com>
2019-05-13 10:31:25 +0300

    Adding helper functions

--- test/Custom/Auth/Email/Handler/LoginSpec.hs
+++ test/Custom/Auth/Email/Handler/LoginSpec.hs
@@ -10,6 +10,29 @@ spec = withApp $
 
   describe "Post request to http://localhost:3000/auth/plugin/email/login" $
 
+    let getCheckR = do
+          get ("http://localhost:3000/auth/check" :: Text)
+          statusIs 200
+
+    let basicRequestBuilder encoded = do
+          setMethod "POST"
+          setUrl $ AuthR $ PluginR "email" ["login"]
+          setRequestBody encoded
+          addRequestHeader ("Content-Type", "application/json")
+
+    let postForgotPasswordR encoded =
+          request $ basicRequestBuilder encoded
+
+    let postForgotPasswordRWithToken encoded =
+          request $ do
+            basicRequestBuilder encoded
+            addTokenFromCookie
+
+    let email        = "example@gmail.com" :: Text
+        password     = "password"
+        body         = object [ "email" .= email, "password" .= password ]
+        encoded      = encode body
+
     it "gives a 200 and the body contains \"message\":\"Login successful\"" $ do
 
       let email = "steve.papadogiannis1992@gmail.com" :: Text
